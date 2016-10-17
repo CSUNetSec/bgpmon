6adb472346765e5ab1e10b973f5483b0d7080534
ramrunner <dsp@2f30.org>
2016-10-17 15:20:59 -0600

    changing cassandra protoversion to 3

--- session/cassandra.go
+++ session/cassandra.go
@@ -3,13 +3,13 @@ package session
 import (
 	"errors"
 	"fmt"
-    "net"
+	"net"
 	"time"
 
 	pb "github.com/CSUNetSec/bgpmon/protobuf"
 
 	"github.com/gocql/gocql"
-    //"github.com/golang/protobuf/proto"
+	//"github.com/golang/protobuf/proto"
 )
 
 type CassandraConfig struct {
@@ -17,8 +17,8 @@ type CassandraConfig struct {
 }
 
 type WriterConfig struct {
-	Keyspace            string
-    TimeBucketSeconds   int64
+	Keyspace          string
+	TimeBucketSeconds int64
 }
 
 type CassandraSession struct {
@@ -29,7 +29,7 @@ type CassandraSession struct {
 func NewCassandraSession(username, password string, hosts []string, workerCount uint32, config CassandraConfig) (Sessioner, error) {
 	cluster := gocql.NewCluster(hosts...)
 	//cluster.Consistency = gocql.LocalOne
-	cluster.ProtoVersion = 4
+	cluster.ProtoVersion = 3
 	cluster.RetryPolicy = &gocql.SimpleRetryPolicy{10}
 	cluster.Authenticator = gocql.PasswordAuthenticator{Username: username, Password: password}
 	cluster.NumConns = 16
@@ -59,12 +59,12 @@ func NewCassandraSession(username, password string, hosts []string, workerCount 
 		}
 	}
 
-    session, err := NewSession(writers, workerCount)
-    if err != nil {
-        return nil, err
-    }
+	session, err := NewSession(writers, workerCount)
+	if err != nil {
+		return nil, err
+	}
 
-    return CassandraSession{&session, cqlSession}, nil
+	return CassandraSession{&session, cqlSession}, nil
 }
 
 func (c CassandraSession) Close() error {
@@ -85,11 +85,11 @@ func addWriter(writers map[pb.WriteRequest_Type][]Writer, writeRequestType pb.Wr
  */
 
 const (
-    bgpUpdateMsgByTimeStmt = "INSERT INTO %s.update_messages_by_time(time_bucket, timestamp, advertised_prefixes, as_path, collector_ip_address, collector_mac_address, collector_port, next_hop, peer_ip_address, withdrawn_prefixes) VALUES(?,?,?,?,?,?,?,?,?,?)"
-    bgpUpdateMsgByPrefixRangeStmt = "INSERT INTO %s.as_number_by_prefix_range(time_bucket, prefix_ip_address, prefix_mask, timestamp, as_number) VALUES(?,?,?,?,?)"
-	locationByASStmt = "INSERT INTO %s.location_by_as_number(as_number, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
-	locationByIPAddressStmt = "INSERT INTO %s.location_by_ip_address(ip_address, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
-	locationByPrefixStmt = "INSERT INTO %s.location_by_prefix(prefix_ip_address, prefix_mask, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?,?)"
+	bgpUpdateMsgByTimeStmt        = "INSERT INTO %s.update_messages_by_time(time_bucket, timestamp, advertised_prefixes, as_path, collector_ip_address, collector_mac_address, collector_port, next_hop, peer_ip_address, withdrawn_prefixes) VALUES(?,?,?,?,?,?,?,?,?,?)"
+	bgpUpdateMsgByPrefixRangeStmt = "INSERT INTO %s.as_number_by_prefix_range(time_bucket, prefix_ip_address, prefix_mask, timestamp, as_number) VALUES(?,?,?,?,?)"
+	locationByASStmt              = "INSERT INTO %s.location_by_as_number(as_number, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
+	locationByIPAddressStmt       = "INSERT INTO %s.location_by_ip_address(ip_address, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
+	locationByPrefixStmt          = "INSERT INTO %s.location_by_prefix(prefix_ip_address, prefix_mask, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?,?)"
 )
 
 type CassandraWriter struct {
@@ -99,89 +99,89 @@ type CassandraWriter struct {
 
 type BGPUpdateMsgByTime struct {
 	CassandraWriter
-    timeBucketSeconds int64
+	timeBucketSeconds int64
 }
 
 type IPPrefix struct {
-    IP   net.IP `cql:"ip_address"`
-    Mask uint8  `cql:"mask"`
+	IP   net.IP `cql:"ip_address"`
+	Mask uint8  `cql:"mask"`
 }
 
 func (b BGPUpdateMsgByTime) Write(request *pb.WriteRequest) error {
-    //get message and convert timestamp to timeuuid
-    msg := request.GetBgpUpdateMessage()
-    timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
+	//get message and convert timestamp to timeuuid
+	msg := request.GetBgpUpdateMessage()
+	timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
 
-    advertisedPrefixes := []IPPrefix{}
-    for _, ipPrefix := range msg.AdvertisedPrefixes {
-        advertisedPrefixes = append(advertisedPrefixes, IPPrefix{net.ParseIP(ipPrefix.PrefixIpAddress), uint8(ipPrefix.PrefixMask)})
-    }
+	advertisedPrefixes := []IPPrefix{}
+	for _, ipPrefix := range msg.AdvertisedPrefixes {
+		advertisedPrefixes = append(advertisedPrefixes, IPPrefix{net.ParseIP(ipPrefix.PrefixIpAddress), uint8(ipPrefix.PrefixMask)})
+	}
 
-    withdrawnPrefixes := []IPPrefix{}
-    for _, ipPrefix := range msg.WithdrawnPrefixes {
-        withdrawnPrefixes = append(withdrawnPrefixes, IPPrefix{net.ParseIP(ipPrefix.PrefixIpAddress), uint8(ipPrefix.PrefixMask)})
-    }
+	withdrawnPrefixes := []IPPrefix{}
+	for _, ipPrefix := range msg.WithdrawnPrefixes {
+		withdrawnPrefixes = append(withdrawnPrefixes, IPPrefix{net.ParseIP(ipPrefix.PrefixIpAddress), uint8(ipPrefix.PrefixMask)})
+	}
 
-    err := b.cqlSession.Query(
-			fmt.Sprintf(bgpUpdateMsgByTimeStmt, b.keyspace),
-            time.Unix(msg.Timestamp - (msg.Timestamp % b.timeBucketSeconds), 0),
-            timestamp,
-            advertisedPrefixes, //announced_prefixes
-            msg.AsPath, //as_path
-            net.ParseIP(msg.CollectorIpAddress), //collector_ip_address
-            msg.CollectorMacAddress, //collector_mac_address
-            msg.CollectorPort, //collector_port
-            nil, //TODO msg.NextHop next_hop
-            net.ParseIP(msg.PeerIpAddress), //peer_ip_address
-            withdrawnPrefixes, //withdrawn_prefixes
-		).Exec()
+	err := b.cqlSession.Query(
+		fmt.Sprintf(bgpUpdateMsgByTimeStmt, b.keyspace),
+		time.Unix(msg.Timestamp-(msg.Timestamp%b.timeBucketSeconds), 0),
+		timestamp,
+		advertisedPrefixes,                  //announced_prefixes
+		msg.AsPath,                          //as_path
+		net.ParseIP(msg.CollectorIpAddress), //collector_ip_address
+		msg.CollectorMacAddress,             //collector_mac_address
+		msg.CollectorPort,                   //collector_port
+		nil,                                 //TODO msg.NextHop next_hop
+		net.ParseIP(msg.PeerIpAddress), //peer_ip_address
+		withdrawnPrefixes,              //withdrawn_prefixes
+	).Exec()
 
-    if err != nil {
-        fmt.Printf("aprefix:%v\naspath:%v\ncip:%v\ncmac:%v\ncport:%v\nnhop:%v\npip:%v\nwprefix:%v\n--------------------------\n",
-            advertisedPrefixes,
-            msg.AsPath,
-            net.ParseIP(msg.CollectorIpAddress),
-            msg.CollectorMacAddress,
-            msg.CollectorPort,
-            msg.NextHop,
-            net.ParseIP(msg.PeerIpAddress),
-            withdrawnPrefixes)
+	if err != nil {
+		fmt.Printf("aprefix:%v\naspath:%v\ncip:%v\ncmac:%v\ncport:%v\nnhop:%v\npip:%v\nwprefix:%v\n--------------------------\n",
+			advertisedPrefixes,
+			msg.AsPath,
+			net.ParseIP(msg.CollectorIpAddress),
+			msg.CollectorMacAddress,
+			msg.CollectorPort,
+			msg.NextHop,
+			net.ParseIP(msg.PeerIpAddress),
+			withdrawnPrefixes)
 
-        panic(err)
-    }
+		panic(err)
+	}
 
-    return nil
+	return nil
 }
 
 type BGPUpdateMsgByPrefixRange struct {
 	CassandraWriter
-    timeBucketSeconds int64
+	timeBucketSeconds int64
 }
 
 func (b BGPUpdateMsgByPrefixRange) Write(request *pb.WriteRequest) error {
-    //get message and convert timestamp to timeuuid
-    msg := request.GetBgpUpdateMessage()
-    timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
+	//get message and convert timestamp to timeuuid
+	msg := request.GetBgpUpdateMessage()
+	timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
 
-    for _, prefix := range msg.AdvertisedPrefixes {
-        //parse ip address
-        prefixIP := net.ParseIP(prefix.PrefixIpAddress)
+	for _, prefix := range msg.AdvertisedPrefixes {
+		//parse ip address
+		prefixIP := net.ParseIP(prefix.PrefixIpAddress)
 
-        err := b.cqlSession.Query(
-                fmt.Sprintf(bgpUpdateMsgByPrefixRangeStmt, b.keyspace),
-                time.Unix(msg.Timestamp - (msg.Timestamp % b.timeBucketSeconds), 0),
-                prefixIP,
-                prefix.PrefixMask,
-                timestamp,
-                msg.AsPath[len(msg.AsPath)-1],
-            ).Exec()
+		err := b.cqlSession.Query(
+			fmt.Sprintf(bgpUpdateMsgByPrefixRangeStmt, b.keyspace),
+			time.Unix(msg.Timestamp-(msg.Timestamp%b.timeBucketSeconds), 0),
+			prefixIP,
+			prefix.PrefixMask,
+			timestamp,
+			msg.AsPath[len(msg.AsPath)-1],
+		).Exec()
 
-        if err != nil {
-            return err
-        }
-    }
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
 
 type LocationByAS struct {
@@ -197,16 +197,16 @@ func (l LocationByAS) Write(request *pb.WriteRequest) error {
 	}
 
 	return l.cqlSession.Query(
-			fmt.Sprintf(locationByASStmt, l.keyspace),
-			msg.AsNumber,
-			gocql.UUIDFromTime(measureDate),
-			location.CountryCode,
-			location.StateCode,
-			location.City,
-			float32(location.Latitude),
-			float32(location.Longitude),
-			msg.Source,
-		).Exec()
+		fmt.Sprintf(locationByASStmt, l.keyspace),
+		msg.AsNumber,
+		gocql.UUIDFromTime(measureDate),
+		location.CountryCode,
+		location.StateCode,
+		location.City,
+		float32(location.Latitude),
+		float32(location.Longitude),
+		msg.Source,
+	).Exec()
 }
 
 type LocationByIPAddress struct {
@@ -222,16 +222,16 @@ func (l LocationByIPAddress) Write(request *pb.WriteRequest) error {
 	}
 
 	return l.cqlSession.Query(
-			fmt.Sprintf(locationByIPAddressStmt, l.keyspace),
-			msg.IpAddress,
-			gocql.UUIDFromTime(measureDate),
-			location.CountryCode,
-			location.StateCode,
-			location.City,
-			float32(location.Latitude),
-			float32(location.Longitude),
-			msg.Source,
-		).Exec()
+		fmt.Sprintf(locationByIPAddressStmt, l.keyspace),
+		msg.IpAddress,
+		gocql.UUIDFromTime(measureDate),
+		location.CountryCode,
+		location.StateCode,
+		location.City,
+		float32(location.Latitude),
+		float32(location.Longitude),
+		msg.Source,
+	).Exec()
 }
 
 type LocationByPrefix struct {
@@ -247,15 +247,15 @@ func (l LocationByPrefix) Write(request *pb.WriteRequest) error {
 	}
 
 	return l.cqlSession.Query(
-			fmt.Sprintf(locationByPrefixStmt, l.keyspace),
-			msg.Prefix.PrefixIpAddress,
-			msg.Prefix.PrefixMask,
-			gocql.UUIDFromTime(measureDate),
-			location.CountryCode,
-			location.StateCode,
-			location.City,
-			float32(location.Latitude),
-			float32(location.Longitude),
-			msg.Source,
-		).Exec()
+		fmt.Sprintf(locationByPrefixStmt, l.keyspace),
+		msg.Prefix.PrefixIpAddress,
+		msg.Prefix.PrefixMask,
+		gocql.UUIDFromTime(measureDate),
+		location.CountryCode,
+		location.StateCode,
+		location.City,
+		float32(location.Latitude),
+		float32(location.Longitude),
+		msg.Source,
+	).Exec()
 }
