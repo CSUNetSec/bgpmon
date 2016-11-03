46273e13d40d9755c98698c38d3218bd04fc307e
ramrunner <dsp@2f30.org>
2016-11-02 19:30:29 -0600

    server side implementing write of BGPCapture types

--- session/cassandra.go
+++ session/cassandra.go
@@ -7,6 +7,9 @@ import (
 	"time"
 
 	pb "github.com/CSUNetSec/netsec-protobufs/bgpmon"
+	pbcom "github.com/CSUNetSec/netsec-protobufs/common"
+	pbbgp "github.com/CSUNetSec/netsec-protobufs/protocol/bgp"
+	"github.com/golang/protobuf/proto"
 
 	"github.com/gocql/gocql"
 	//"github.com/golang/protobuf/proto"
@@ -30,6 +33,32 @@ type CassandraSession struct {
 	CqlSession *gocql.Session
 }
 
+func parseIp(a pbcom.IPAddressWrapper) (net.IP, error) {
+	if len(a.Ipv6) != 0 {
+		return net.IP(a.Ipv6), nil
+	} else if len(a.Ipv4) != 0 {
+		return net.IP(a.Ipv4).To4(), nil
+	} else {
+		return nil, fmt.Errorf("IP Prefix that is neither v4 or v6 found in BGPCapture msg")
+	}
+}
+
+func getAsPath(a pbbgp.BGPUpdate) []uint32 {
+	ret := []uint32{}
+	if a.Attrs != nil {
+		for _, seg := range a.Attrs.AsPath {
+			if seg.AsSeq != nil {
+				ret = append(ret, seg.AsSeq...)
+			}
+			if seg.AsSet != nil {
+				ret = append(ret, seg.AsSet...)
+			}
+
+		}
+	}
+	return ret
+}
+
 func NewCassandraSession(username, password string, hosts []string, workerCount uint32, config CassandraConfig) (Sessioner, error) {
 	cluster := gocql.NewCluster(hosts...)
 	//cluster.Consistency = gocql.LocalOne
@@ -51,6 +80,10 @@ func NewCassandraSession(username, password string, hosts []string, workerCount 
 				addWriter(writers, pb.WriteRequest_BGP_UPDATE, BGPUpdateMsgByTime{CassandraWriter{cqlSession, writerConfig.Keyspace}, writerConfig.TimeBucketSeconds})
 			case "BGPUpdateMsgByPrefixRange":
 				addWriter(writers, pb.WriteRequest_BGP_UPDATE, BGPUpdateMsgByPrefixRange{CassandraWriter{cqlSession, writerConfig.Keyspace}, writerConfig.TimeBucketSeconds})
+			case "BGPCaptureByTime":
+				addWriter(writers, pb.WriteRequest_BGP_CAPTURE, BGPCaptureByTime{CassandraWriter{cqlSession, writerConfig.Keyspace}, writerConfig.TimeBucketSeconds})
+			case "BGPCaptureByPrefixRange":
+				addWriter(writers, pb.WriteRequest_BGP_CAPTURE, BGPCaptureByPrefixRange{CassandraWriter{cqlSession, writerConfig.Keyspace}, writerConfig.TimeBucketSeconds})
 			case "LocationByAS":
 				addWriter(writers, pb.WriteRequest_AS_NUMBER_LOCATION, LocationByAS{CassandraWriter{cqlSession, writerConfig.Keyspace}})
 			case "LocationByIPAddress":
@@ -91,6 +124,8 @@ func addWriter(writers map[pb.WriteRequest_Type][]Writer, writeRequestType pb.Wr
 const (
 	bgpUpdateMsgByTimeStmt        = "INSERT INTO %s.update_messages_by_time(time_bucket, timestamp, advertised_prefixes, as_path, collector_ip_address, collector_mac_address, collector_port, next_hop, peer_ip_address, withdrawn_prefixes) VALUES(?,?,?,?,?,?,?,?,?,?)"
 	bgpUpdateMsgByPrefixRangeStmt = "INSERT INTO %s.as_number_by_prefix_range(time_bucket, prefix_ip_address, prefix_mask, timestamp, as_number) VALUES(?,?,?,?,?)"
+	bgpCaptureByTimeStmt          = "INSERT INTO %s.update_messages_by_time(time_bucket, timestamp, advertised_prefixes, as_path, collector_ip_address, next_hop, peer_ip_address, withdrawn_prefixes, protomsg) VALUES(?,?,?,?,?,?,?,?,?)"
+	bgpCaptureByPrefixRangeStmt   = "INSERT INTO %s.as_number_by_prefix_range(time_bucket, prefix_ip_address, prefix_mask, timestamp, as_number) VALUES(?,?,?,?,?)"
 	locationByASStmt              = "INSERT INTO %s.location_by_as_number(as_number, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
 	locationByIPAddressStmt       = "INSERT INTO %s.location_by_ip_address(ip_address, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?)"
 	locationByPrefixStmt          = "INSERT INTO %s.location_by_prefix(prefix_ip_address, prefix_mask, measure_date, country_code, state_code, city, latitude, longitude, source) VALUES(?,?,?,?,?,?,?,?,?)"
@@ -277,4 +312,155 @@ func (l LocationByPrefix) Write(request *pb.WriteRequest) error {
 		float32(location.Longitude),
 		msg.Source,
 	).Exec()
+}
+
+type BGPCaptureByTime struct {
+	CassandraWriter
+	timeBucketSeconds int64
+}
+
+type BGPCaptureByPrefixRange struct {
+	CassandraWriter
+	timeBucketSeconds int64
+}
+
+func (b BGPCaptureByTime) Write(request *pb.WriteRequest) error {
+	//get message and convert timestamp to timeuuid
+	errcount := 0
+	msg := request.GetBgpCapture()
+	if msg == nil {
+		return fmt.Errorf("BGPCapture WriteRequest message was nil")
+	}
+	msgUp := msg.GetUpdate()
+	if msgUp == nil {
+		return fmt.Errorf("BGPUpdate in BGPCapture message was nil")
+	}
+
+	timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
+	advertisedPrefixes := []IPPrefix{}
+	withdrawnPrefixes := []IPPrefix{}
+	if msgUp.WithdrawnRoutes != nil {
+		if len(msgUp.WithdrawnRoutes.Prefixes) != 0 {
+			for _, wr := range msgUp.WithdrawnRoutes.Prefixes {
+				ip, err := parseIp(*wr.Prefix)
+				if err != nil {
+					return err
+				}
+				withdrawnPrefixes = append(withdrawnPrefixes, IPPrefix{ip, uint8(wr.Mask)})
+			}
+		}
+	}
+	if msgUp.AdvertizedRoutes != nil {
+		if len(msgUp.AdvertizedRoutes.Prefixes) != 0 {
+			for _, ar := range msgUp.AdvertizedRoutes.Prefixes {
+				ip, err := parseIp(*ar.Prefix)
+				if err != nil {
+					return err
+				}
+				advertisedPrefixes = append(advertisedPrefixes, IPPrefix{ip, uint8(ar.Mask)})
+			}
+		}
+	}
+	asp := getAsPath(*msgUp)
+	colip, err := parseIp(*msg.LocalIp)
+	if err != nil {
+		return fmt.Errorf("Capture Local IP parsing error:%s", err)
+	}
+	peerip, err := parseIp(*msg.PeerIp)
+	if err != nil {
+		return fmt.Errorf("Capture Peer IP parsing error:%s", err)
+	}
+	var nhip net.IP
+	if msgUp.Attrs != nil {
+		if msgUp.Attrs.NextHop != nil {
+			var err error
+			nhip, err = parseIp(*msgUp.Attrs.NextHop)
+			if err != nil {
+				return fmt.Errorf("Capture NextHop IP parsing error:%s", err)
+			}
+		}
+	}
+	capbytes, err := proto.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("failed to serialize BGPCapture proto:%s", err)
+	}
+
+retry:
+	if errcount < writeretries {
+		err := b.cqlSession.Query(
+			fmt.Sprintf(bgpCaptureByTimeStmt, b.keyspace),
+			time.Unix(int64(msg.Timestamp)-(int64(msg.Timestamp)%b.timeBucketSeconds), 0),
+			timestamp,
+			advertisedPrefixes, //announced_prefixes
+			asp,                //as_path
+			colip,              //collector_ip_address
+			nhip,               //NextHop next_hop
+			peerip,             //peer_ip_address
+			withdrawnPrefixes,  //withdrawn_prefixes
+			capbytes,           //msg bytes
+		).Exec()
+		if err != nil {
+			fmt.Printf("aprefix:%v\naspath:%v\ncip:%v\nnhop:%v\npip:%v\nwprefix:%v\n--------------------------\n",
+				advertisedPrefixes,
+				asp,
+				colip,
+				nhip,
+				peerip,
+				withdrawnPrefixes)
+			errcount++
+			goto retry
+		}
+	}
+
+	return nil
+}
+
+func (b BGPCaptureByPrefixRange) Write(request *pb.WriteRequest) error {
+	errcount := 0
+	//get message and convert timestamp to timeuuid
+	msg := request.GetBgpCapture()
+	if msg == nil {
+		return fmt.Errorf("BGPCapture WriteRequest message was nil")
+	}
+	msgUp := msg.GetUpdate()
+	if msgUp == nil {
+		return fmt.Errorf("BGPUpdate in BGPCapture message was nil")
+	}
+	asp := getAsPath(*msgUp)
+	if len(asp) == 0 { // no as path info in that update
+		return nil
+	}
+	timestamp := gocql.UUIDFromTime(time.Unix(int64(msg.Timestamp), 0))
+	//fmt.Printf("by prefix msg with timestamp :%s\n", timestamp)
+
+	if msgUp.AdvertizedRoutes != nil {
+		if len(msgUp.AdvertizedRoutes.Prefixes) != 0 {
+			for _, ar := range msgUp.AdvertizedRoutes.Prefixes {
+				ip, err := parseIp(*ar.Prefix)
+				if err != nil {
+					return err
+				}
+			retry:
+				if errcount < writeretries {
+					err = b.cqlSession.Query(
+						fmt.Sprintf(bgpCaptureByPrefixRangeStmt, b.keyspace),
+						time.Unix(int64(msg.Timestamp)-(int64(msg.Timestamp)%b.timeBucketSeconds), 0),
+						ip,
+						ar.Mask,
+						timestamp,
+						asp[len(asp)-1],
+					).Exec()
+
+					if err != nil {
+						errcount++
+						goto retry
+					}
+				} else {
+					return err
+				}
+
+			}
+		}
+	}
+	return nil
 }
