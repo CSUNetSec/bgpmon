c9e44b61a02df283bae3cef1f293ecee6447d130
dsp <dsp@2f30.org>
2017-01-05 17:47:51 -0700

    adding utility dir with some code for time/duration management

---
+++ util/time.go
@@ -0,0 +1,45 @@
+//Utility functions for the handling of time related tasks
+package util
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	errtimeorder = errors.New("start time is later than end time")
+	errzeroquant = errors.New("Quantum can't be zero")
+)
+
+type TimeRangeGenerator struct {
+	start   time.Time
+	end     time.Time
+	quantum time.Duration
+	cur     time.Time
+}
+
+func NewTimeRangeGenerator(start, end time.Time, quantum time.Duration) (*TimeRangeGenerator, error) {
+	if start.After(end) {
+		return nil, errtimeorder
+	}
+	if quantum == time.Duration(0) {
+		return nil, errzeroquant
+	}
+	if quantum < 0 {
+		return &TimeRangeGenerator{end, start, quantum, end}, nil
+	}
+	return &TimeRangeGenerator{start, end, quantum, start}, nil
+}
+
+func (t TimeRangeGenerator) Next() bool {
+	if t.cur.Add(t.quantum).After(t.end) {
+		return false
+	}
+	return true
+}
+
+func (t *TimeRangeGenerator) DatePair() (A, B time.Time) {
+	A, B = t.cur, t.cur.Add(t.quantum)
+	t.cur = B
+	return
+}
