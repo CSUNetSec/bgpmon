627eba1b754824d35c490d9d2664a7b3e6359ae1
Will Yingling <willyingling@gmail.com>
2018-08-27 10:11:21 -0600

    Moved some structs and funcs around

--- v2/db/db.go
+++ v2/db/db.go
@@ -1,9 +1,100 @@
 package db
 
 import (
+	"context"
+	"database/sql"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 var (
 	dblogger = logrus.WithField("system", "db")
 )
+
+type Dber interface {
+	Db() *sql.DB
+}
+
+// sqlCtxExecutor is an interface needed for basic queries.
+// It is implemented partly by both sql.DB and sql.Txn but we will
+// implemented on our wrapper ctxTx. ctxTx wraps these functions with a context
+// and deeper their xxxContext variant is called.
+type sqlCtxExecutor interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
+//creates a new ctxTx for that operation which implements the
+//sqlExecutor interface. The argument passed instructs it to either
+//do it on a transaction if true, or on the normal DB connection if false.
+//caller must call Done() that releases resources.
+func GetNewExecutor(pc context.Context, s Dber, doTx bool, ctxTimeout time.Duration) (*ctxTx, error) {
+	var (
+		tx  *sql.Tx
+		err error
+		db  *sql.DB
+	)
+	db = s.Db()
+	ctx, cf := context.WithTimeout(pc, ctxTimeout)
+	if doTx {
+		if tx, err = db.BeginTx(ctx, nil); err != nil {
+			cf()
+			return nil, err
+		}
+	} else {
+		tx = nil
+	}
+	return &ctxTx{
+		doTx: doTx,
+		tx:   tx,
+		cf:   cf,
+		ctx:  ctx,
+		db:   db,
+	}, nil
+}
+
+func (c *ctxTx) Exec(query string, args ...interface{}) (sql.Result, error) {
+	if c.doTx && c.tx != nil {
+		return c.tx.ExecContext(c.ctx, query, args...)
+	}
+	return c.db.ExecContext(c.ctx, query, args...)
+}
+
+func (c *ctxTx) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	if c.doTx && c.tx != nil {
+		return c.tx.QueryContext(c.ctx, query, args...)
+	}
+	return c.db.QueryContext(c.ctx, query, args...)
+}
+
+func (c *ctxTx) QueryRow(query string, args ...interface{}) *sql.Row {
+	if c.doTx && c.tx != nil {
+		return c.tx.QueryRowContext(c.ctx, query, args...)
+	}
+	return c.db.QueryRowContext(c.ctx, query, args...)
+}
+
+//a wrapper of a sql.Tx that is able to accept multiple
+//db ops and run them in the same tx.
+//it will implement the sqlCtxExectutor interface and choose
+//where to apply the sql function depending on how it was constructed.
+//(either apply everything in the transaction and then the last Done()
+//will commit, or straight on the DB and the last Done() is a noop.
+//the ctxTx structs are created by the specific sessions.
+type ctxTx struct {
+	doTx bool
+	tx   *sql.Tx
+	db   *sql.DB
+	cf   context.CancelFunc
+	ctx  context.Context
+}
+
+//either commits the TX or just releases the context through it's cancelfunc.
+func (ptx *ctxTx) Done() error {
+	defer ptx.cf() //release resources if it's done.
+	if ptx.doTx && ptx.tx != nil {
+		return ptx.tx.Commit()
+	}
+	return nil
+}
--- v2/db/session.go
+++ v2/db/session.go
@@ -15,90 +15,6 @@ const (
 	CTXTIMEOUT = time.Duration(10) * time.Second
 )
 
-// sqlCtxExecutor is an interface needed for basic queries.
-// It is implemented partly by both sql.DB and sql.Txn but we will
-// implemented on our wrapper ctxTx. ctxTx wraps these functions with a context
-// and deeper their xxxContext variant is called.
-type sqlCtxExecutor interface {
-	Exec(query string, args ...interface{}) (sql.Result, error)
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-	QueryRow(query string, args ...interface{}) *sql.Row
-}
-
-//creates a new ctxTx for that operation which implements the
-//sqlExecutor interface. The argument passed instructs it to either
-//do it on a transaction if true, or on the normal DB connection if false.
-//caller must call Done() that releases resources.
-func GetNewExecutor(pc context.Context, s Dber, doTx bool, ctxTimeout time.Duration) (*ctxTx, error) {
-	var (
-		tx  *sql.Tx
-		err error
-		db  *sql.DB
-	)
-	db = s.Db()
-	ctx, cf := context.WithTimeout(pc, ctxTimeout)
-	if doTx {
-		if tx, err = db.BeginTx(ctx, nil); err != nil {
-			cf()
-			return nil, err
-		}
-	} else {
-		tx = nil
-	}
-	return &ctxTx{
-		doTx: doTx,
-		tx:   tx,
-		cf:   cf,
-		ctx:  ctx,
-		db:   db,
-	}, nil
-}
-
-func (c *ctxTx) Exec(query string, args ...interface{}) (sql.Result, error) {
-	if c.doTx && c.tx != nil {
-		return c.tx.ExecContext(c.ctx, query, args...)
-	}
-	return c.db.ExecContext(c.ctx, query, args...)
-}
-
-func (c *ctxTx) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	if c.doTx && c.tx != nil {
-		return c.tx.QueryContext(c.ctx, query, args...)
-	}
-	return c.db.QueryContext(c.ctx, query, args...)
-}
-
-func (c *ctxTx) QueryRow(query string, args ...interface{}) *sql.Row {
-	if c.doTx && c.tx != nil {
-		return c.tx.QueryRowContext(c.ctx, query, args...)
-	}
-	return c.db.QueryRowContext(c.ctx, query, args...)
-}
-
-//a wrapper of a sql.Tx that is able to accept multiple
-//db ops and run them in the same tx.
-//it will implement the sqlCtxExectutor interface and choose
-//where to apply the sql function depending on how it was constructed.
-//(either apply everything in the transaction and then the last Done()
-//will commit, or straight on the DB and the last Done() is a noop.
-//the ctxTx structs are created by the specific sessions.
-type ctxTx struct {
-	doTx bool
-	tx   *sql.Tx
-	db   *sql.DB
-	cf   context.CancelFunc
-	ctx  context.Context
-}
-
-//either commits the TX or just releases the context through it's cancelfunc.
-func (ptx *ctxTx) Done() error {
-	defer ptx.cf() //release resources if it's done.
-	if ptx.doTx && ptx.tx != nil {
-		return ptx.tx.Commit()
-	}
-	return nil
-}
-
 type sessionCmd int
 
 const (
@@ -112,14 +28,22 @@ type Sessioner interface {
 
 type Session struct {
 	uuid     string
-	workerCt int
+	maxWk    int
 	activeWk int
 	lock     *sync.Mutex
+	ctx      context.Context
 }
 
 // Maybe this should return a channel that the calling function
 // could read from to get the reply
 func (s *Session) Do(cmd sessionCmd, arg interface{}) (interface{}, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if s.activeWk >= s.maxWk {
+		// Receive from a 'done' channel
+	}
+
 	return nil, nil
 }
 
@@ -127,14 +51,10 @@ func (s *Session) Close() error {
 	return nil
 }
 
-type Dber interface {
-	Db() *sql.DB
-}
-
 func NewSession(ctx context.Context, conf config.SessionConfiger, id string, nworkers int) (Sessioner, error) {
 
 	var err error
-	s := &Session{uuid: id, workerCt: nworkers, activeWk: 0, lock: &sync.Mutex{}}
+	s := &Session{uuid: id, maxWk: nworkers, activeWk: 0, lock: &sync.Mutex{}, ctx: ctx}
 
 	// The DB will need to be a field within session
 	switch st := conf.GetTypeName(); st {
