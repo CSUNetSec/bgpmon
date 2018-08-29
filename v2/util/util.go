c49ee0019545f95f8cc9adb88330cc079104902b
Will Yingling <willyingling@gmail.com>
2018-08-29 16:38:00 -0600

    Fixed data race

--- v2/util/util.go
+++ v2/util/util.go
@@ -5,49 +5,50 @@ import (
 )
 
 type WorkerPool struct {
-	max    int
-	active int
-	req    chan bool
-	done   chan bool
-	close  chan bool
-	wg     *sync.WaitGroup
-	closed bool
+	max      int
+	active   int
+	req      chan bool
+	done     chan bool
+	close    chan bool
+	workerWg *sync.WaitGroup
+	daemonWg *sync.WaitGroup
 }
 
 func NewWorkerPool(ct int) *WorkerPool {
-	wp := &WorkerPool{max: ct, active: 0, wg: &sync.WaitGroup{}, closed: false}
+	wp := &WorkerPool{max: ct, active: 0, workerWg: &sync.WaitGroup{}, daemonWg: &sync.WaitGroup{}}
 	wp.req = make(chan bool)
 	wp.done = make(chan bool, ct)
 	wp.close = make(chan bool)
 
+	wp.daemonWg.Add(1)
 	go wp.daemon()
 	return wp
 }
 
 func (wp *WorkerPool) Add() {
-	wp.wg.Add(1)
+	wp.workerWg.Add(1)
 	<-wp.req
 }
 
 func (wp *WorkerPool) Done() {
 	wp.done <- true
-	wp.wg.Done()
+	wp.workerWg.Done()
 }
 
-func (wp *WorkerPool) Close() {
-	wp.wg.Wait()
+func (wp *WorkerPool) Close() bool {
+	wp.workerWg.Wait()
 	wp.close <- true
-}
+	wp.daemonWg.Wait()
 
-func (wp *WorkerPool) Closed() bool {
-	return wp.closed
+	return true
 }
 
 func (wp *WorkerPool) daemon() {
+	defer wp.daemonWg.Done()
+
 	for {
 		select {
 		case <-wp.close:
-			wp.closed = true
 			return
 		default:
 		}
