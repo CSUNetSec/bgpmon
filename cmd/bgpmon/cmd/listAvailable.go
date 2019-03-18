8e6eaa674208e1277a5a76b3daf06cd1d3635256
dsp <dsp@2f30.org>
2019-03-18 13:45:02 -0600

    codereview fixes on listAvailable

--- cmd/bgpmon/cmd/listAvailable.go
+++ cmd/bgpmon/cmd/listAvailable.go
@@ -7,34 +7,34 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// listAvailableCmd represents the listAvailable command
 var listAvailableCmd = &cobra.Command{
 	Use:   "listAvailable",
-	Short: "list configured types of sessions and their names",
-	Long: `Lists the names and information of the sessions configured in a bgpmond
-these names can be used with the open command to open these types of sessions`,
+	Short: "Lists configured types of sessions and their names.",
+	Long: `Lists the names and information of the sessions configured in a bgpmond server.
+These names can be used with the open command to open these types of sessions.`,
 	Args: cobra.NoArgs,
-	Run:  listAvail,
+	Run:  listAvailable,
 }
 
-func listAvail(cmd *cobra.Command, args []string) {
-	fmt.Printf("listAvailable called with args:%s\n", args)
-	if bc, clierr := newBgpmonCli(bgpmondHost, bgpmondPort); clierr != nil {
+// the first argument is required by cobra but not used.
+func listAvailable(_ *cobra.Command, args []string) {
+	bc, clierr := newBgpmonCli(bgpmondHost, bgpmondPort)
+	if clierr != nil {
 		fmt.Printf("Error: %s\n", clierr)
-	} else {
-		defer bc.close()
-		emsg := &pb.Empty{}
-		ctx, cancel := getCtxWithCancel()
-		defer cancel()
-		if reply, err := bc.cli.ListAvailableSessions(ctx, emsg); err != nil {
-			fmt.Printf("Error: %s\n", err)
-		} else {
-			fmt.Printf("%d Available Sessions:\n", len(reply.AvailableSessions))
-			for i, as := range reply.AvailableSessions {
-				fmt.Printf("[%d]\n\tName:%s\n\tType:%s\n\tDescription:%v\n", i, as.Name, as.Type, as.Desc)
-			}
-		}
-
+		return
+	}
+	defer bc.close()
+	emsg := &pb.Empty{}
+	ctx, cancel := getCtxWithCancel()
+	defer cancel()
+	reply, err := bc.cli.ListAvailableSessions(ctx, emsg)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+	fmt.Printf("%d Available Sessions:\n", len(reply.AvailableSessions))
+	for i, as := range reply.AvailableSessions {
+		fmt.Printf("[%d]\n\tName:%s\n\tType:%s\n\tDescription:%v\n", i, as.Name, as.Type, as.Desc)
 	}
 }
 
