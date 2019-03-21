6ae03e06d4f0f5cdcb9ea1d9e7b3dac0d1ffa34e
dsp <dsp@2f30.org>
2019-03-21 16:00:16 -0600

    bringing db.go closer to codereview standards

--- db/db.go
+++ db/db.go
@@ -1,3 +1,8 @@
+// Package db is responsible for the communication between the database backends and the bgpmon daemon.
+// It abstracts database operations through types that are named Executors. Executors allow for one off, or
+// transactional operations, that can be timed out using a context, and can be database unware by getting
+// the approprate SQL statement depending what type of database exists on the back.
+
 package db
 
 import (
@@ -11,10 +16,12 @@ import (
 	"github.com/CSUNetSec/bgpmon/util"
 )
 
+// This block holds the currently supported database backends.
 const (
 	postgres = iota
 )
 
+// This block holds the supported operations on the databases.
 const (
 	connectNoSSLOp       = "connectNoSSL"
 	connectSSLOp         = "connectSSL"
@@ -30,17 +37,19 @@ const (
 	getCaptureTablesOp   = "getCaptureTablesTmpl"
 )
 
+// dbops associates every generic database operation with an array that holds the correct SQL statements
+// for each type of database. The index in the value array is controlled by the database const.
 var dbops = map[string][]string{
 	connectNoSSLOp: {
-		//postgres
+		// postgres
 		`user=%s password=%s dbname=%s host=%s sslmode=disable`,
 	},
 	connectSSLOp: {
-		//postgres
+		// postgres
 		`user=%s password=%s dbname=%s host=%s`,
 	},
 	checkSchemaOp: {
-		//postgres
+		// postgres
 		`SELECT EXISTS (
 		   SELECT *
 		   FROM   information_schema.tables
@@ -48,11 +57,11 @@ var dbops = map[string][]string{
 		 );`,
 	},
 	selectNodeOp: {
-		//postgres
+		// postgres
 		`SELECT name, ip, isCollector, tableDumpDurationMinutes, description, coords, address FROM %s;`,
 	},
 	insertNodeOp: {
-		//postgres
+		// postgres
 		`INSERT INTO %s (name, ip, isCollector, tableDumpDurationMinutes, description, coords, address) 
 		   VALUES ($1, $2, $3, $4, $5, $6, $7)
 		   ON CONFLICT (ip) DO UPDATE SET name=EXCLUDED.name, isCollector=EXCLUDED.isCollector, 
@@ -60,7 +69,7 @@ var dbops = map[string][]string{
 		     description=EXCLUDED.description, coords=EXCLUDED.coords, address=EXCLUDED.address;`,
 	},
 	makeMainTableOp: {
-		//postgres
+		// postgres
 		`CREATE TABLE IF NOT EXISTS %s (
 		   dbname varchar PRIMARY KEY,
 	           collector varchar NOT NULL,
@@ -69,11 +78,11 @@ var dbops = map[string][]string{
                  );`,
 	},
 	insertMainTableOp: {
-		//postgres
+		// postgres
 		`INSERT INTO %s (dbname, collector, dateFrom, dateTo) VALUES ($1, $2, $3, $4);`,
 	},
 	makeCaptureTableOp: {
-		//postgres
+		// postgres
 		`CREATE TABLE IF NOT EXISTS %s (
 		   update_id BIGSERIAL PRIMARY KEY NOT NULL, 
 		   timestamp timestamp NOT NULL,
@@ -88,16 +97,16 @@ var dbops = map[string][]string{
 	},
 	// This template shouldn't need VALUES, because those will be provided by the buffer
 	insertCaptureTableOp: {
-		//postgres
+		// postgres
 		`INSERT INTO %s (timestamp, collector_ip, peer_ip, as_path, next_hop, origin_as, adv_prefixes, wdr_prefixes, protomsg) VALUES `,
 	},
 	selectTableOp: {
-		//postgres
+		// postgres
 		`SELECT dbname, collector, dateFrom, dateTo, tableDumpDurationMinutes FROM %s,%s 
 		 WHERE dateFrom <= $1 AND dateTo > $1 AND ip = $2;`,
 	},
 	makeNodeTableOp: {
-		//postgres
+		// postgres
 		`CREATE TABLE IF NOT EXISTS %s (
 		   ip varchar PRIMARY KEY,
 		   name varchar NOT NULL, 
@@ -113,16 +122,17 @@ var dbops = map[string][]string{
 	},
 }
 
+// dblogger is the logger for the database subsystem.
 var (
 	dblogger = util.NewLogger("system", "db")
 )
 
-//Dber is an interface that returns a reference to the underlying *sql.DB
+// Dber is an interface that returns a reference to the underlying *sql.DB
 type Dber interface {
 	Db() *sql.DB
 }
 
-//TimeoutDber composes Dber and GetTimeouter to return timeout duration for dbops
+// TimeoutDber composes Dber and GetTimeouter to return timeout duration for dbops
 type TimeoutDber interface {
 	Dber
 	util.GetTimeouter
@@ -136,31 +146,39 @@ type dbOper struct {
 	t int
 }
 
-// Gets the specific db op string from the static table declared in db.go
-// for the appropriate dbType that was populated when the correct newSession was called.
-// Panics on error.
-// implementing the getdboper interface in db.go
-func (d *dbOper) getdbop(a string) (ret string) {
-	if sslice, exists := dbops[a]; !exists {
-		panic(fmt.Sprintf("nx db op name:%s requested.", a))
-	} else if len(sslice)-1 < d.t {
-		panic(fmt.Sprintf("dbop:%s for this db type not populated", a))
-	} else {
-		ret = sslice[d.t]
-	}
-	return
-}
-
 func newPostgressDbOper() *dbOper {
 	return &dbOper{
 		t: postgres,
 	}
 }
 
-//SessionExecutor wraps an util.SQLExecutor with getdboper
+// Gets the specific db op string from the static table declared in db.go
+// for the appropriate dbType that was populated when the correct newSession was called.
+// Termintates the application on errors.
+// implementing the getdboper interface in db.go
+func (d *dbOper) getdbop(a string) string {
+	var (
+		sslice []string
+		exists bool
+	)
+	sslice, exists = dbops[a]
+	if !exists {
+		dblogger.Fatalf("nx db op name:%s requested.", a)
+	}
+	if len(sslice)-1 < d.t {
+		dblogger.Fatalf("dbop:%s for this db type not populated", a)
+	}
+	return sslice[d.t]
+}
+
+// SessionExecutor wraps an util.SQLExecutor with getdboper
 type SessionExecutor struct {
 	util.SQLExecutor
 	getdboper
+}
+
+func newSessionExecutor(ex util.SQLExecutor, oper getdboper) SessionExecutor {
+	return SessionExecutor{SQLExecutor: ex, getdboper: oper}
 }
 
 func (s SessionExecutor) getExecutor() util.SQLExecutor {
@@ -171,14 +189,10 @@ func (s SessionExecutor) getOper() getdboper {
 	return s.getdboper
 }
 
-func newSessionExecutor(ex util.SQLExecutor, oper getdboper) SessionExecutor {
-	return SessionExecutor{SQLExecutor: ex, getdboper: oper}
-}
-
-//a wrapper of a sql.Tx that is able to accept multiple
-//db ops and run them in the same tx.
-//it will implement the SQLAtomicExectutor interface and choose
-//where to apply the sql function depending on how it was constructed.
+// a wrapper of a sql.Tx that is able to accept multiple
+// db ops and run them in the same tx.
+// it will implement the SQLAtomicExectutor interface and choose
+// where to apply the sql function depending on how it was constructed.
 type ctxTx struct {
 	doTx bool
 	tx   *sql.Tx
@@ -187,48 +201,9 @@ type ctxTx struct {
 	ctx  context.Context
 }
 
-func (c *ctxTx) Exec(query string, args ...interface{}) (sql.Result, error) {
-	if c.doTx && c.tx != nil {
-		return c.tx.ExecContext(c.ctx, query, args...)
-	}
-	return c.db.ExecContext(c.ctx, query, args...)
-}
-
-func (c *ctxTx) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	if c.doTx && c.tx != nil {
-		return c.tx.QueryContext(c.ctx, query, args...)
-	}
-	return c.db.QueryContext(c.ctx, query, args...)
-}
-
-func (c *ctxTx) QueryRow(query string, args ...interface{}) *sql.Row {
-	if c.doTx && c.tx != nil {
-		return c.tx.QueryRowContext(c.ctx, query, args...)
-	}
-	return c.db.QueryRowContext(c.ctx, query, args...)
-}
-
-func (c *ctxTx) Commit() error {
-	defer c.cf()
-	if !c.doTx || c.tx == nil {
-		return fmt.Errorf("ctxTx can't be committed when not using a transaction")
-	}
-
-	return c.tx.Commit()
-}
-
-func (c *ctxTx) Rollback() error {
-	defer c.cf()
-	if !c.doTx || c.tx == nil {
-		return fmt.Errorf("ctxTx can't be rolled back when not using a transaction")
-	}
-
-	return c.tx.Rollback()
-}
-
-//newCtxExecutor creates a new ctxTx for that operation which implements the
-//SQLAtomicExecutor interface. The argument passed instructs it to either
-//do it on a transaction if true, or on the normal DB connection if false.
+// newCtxExecutor creates a new ctxTx for that operation which implements the
+// SQLAtomicExecutor interface. The argument passed instructs it to either
+// do it on a transaction if true, or on the normal DB connection if false.
 func newCtxExecutor(tdb TimeoutDber, doTx bool) (*ctxTx, error) {
 	var (
 		tx  *sql.Tx
@@ -255,10 +230,54 @@ func newCtxExecutor(tdb TimeoutDber, doTx bool) (*ctxTx, error) {
 	}, nil
 }
 
-//This is a representation of a node that is stored in the database using this fields.
-//a node can be either a collector or a peer, and in case of being a collector it is used
-//to generate the table names that data collected by it are stored. it should be also geolocated.
-//known nodes can be supplied by the config file.
+// Exec makes the CtxExecutor conform to the the sql.Db semantics.
+func (c *ctxTx) Exec(query string, args ...interface{}) (sql.Result, error) {
+	if c.doTx && c.tx != nil {
+		return c.tx.ExecContext(c.ctx, query, args...)
+	}
+	return c.db.ExecContext(c.ctx, query, args...)
+}
+
+// Query makes the CtxExecutor conform to the the sql.Db semantics.
+func (c *ctxTx) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	if c.doTx && c.tx != nil {
+		return c.tx.QueryContext(c.ctx, query, args...)
+	}
+	return c.db.QueryContext(c.ctx, query, args...)
+}
+
+// QueryRow makes the CtxExecutor conform to the the sql.Db semantics.
+func (c *ctxTx) QueryRow(query string, args ...interface{}) *sql.Row {
+	if c.doTx && c.tx != nil {
+		return c.tx.QueryRowContext(c.ctx, query, args...)
+	}
+	return c.db.QueryRowContext(c.ctx, query, args...)
+}
+
+// Commit makes the CtxExecutor conform the sq.Tx semantics.
+func (c *ctxTx) Commit() error {
+	defer c.cf()
+	if !c.doTx || c.tx == nil {
+		return fmt.Errorf("ctxTx can't be committed when not using a transaction")
+	}
+
+	return c.tx.Commit()
+}
+
+// Rollback makes the CtxExecutor conform the sq.Tx semantics.
+func (c *ctxTx) Rollback() error {
+	defer c.cf()
+	if !c.doTx || c.tx == nil {
+		return fmt.Errorf("ctxTx can't be rolled back when not using a transaction")
+	}
+
+	return c.tx.Rollback()
+}
+
+// node is a representation of a machine that is stored in the database using this fields.
+// a node can be either a collector or a peer, and in case of being a collector it is used
+// to generate the table names that data collected by it are stored. it should be also geolocated.
+// known nodes can be supplied by the config file.
 type node struct {
 	nodeName      string
 	nodeIP        string
@@ -269,12 +288,12 @@ type node struct {
 	nodeAddress   string
 }
 
-//creates an empty node
+// newNode creates an empty node.
 func newNode() *node {
 	return &node{}
 }
 
-//creates a nodeconfig from a node
+// nodeConfigFromNode creates a node configuration from a node.
 func (a *node) nodeConfigFromNode() config.NodeConfig {
 	return config.NodeConfig{
 		Name:                a.nodeName,
@@ -287,11 +306,13 @@ func (a *node) nodeConfigFromNode() config.NodeConfig {
 	}
 }
 
+// tableCache provides functions to lookup caches for tables and nodes.
 type tableCache interface {
 	LookupTable(net.IP, time.Time) (string, error)
 	LookupNode(net.IP) (*node, error)
 }
 
+// dbCache implements the tableCache interface.
 type dbCache struct {
 	nodes  map[string]*node
 	tables map[string][]string
@@ -303,6 +324,8 @@ func newDBCache() *dbCache {
 	return &dbCache{nodes: n, tables: t}
 }
 
+// LookupTable provides the name of a table in the db, given the IP of a machine,
+// and a time or an error if it doesn't exist.
 func (dc *dbCache) LookupTable(nodeIP net.IP, t time.Time) (string, error) {
 	ipStr := nodeIP.String()
 	node, ok := dc.nodes[ipStr]
@@ -321,6 +344,7 @@ func (dc *dbCache) LookupTable(nodeIP net.IP, t time.Time) (string, error) {
 	return "", errNoTable
 }
 
+// LookupNode returns the node information given an IP or an error if it doesn't exist.
 func (dc *dbCache) LookupNode(nodeIP net.IP) (*node, error) {
 	ipStr := nodeIP.String()
 	n, ok := dc.nodes[ipStr]
@@ -333,7 +357,7 @@ func (dc *dbCache) LookupNode(nodeIP net.IP) (*node, error) {
 func (dc *dbCache) addTable(nodeIP net.IP, t time.Time) {
 	n, err := dc.LookupNode(nodeIP)
 	if err != nil {
-		fmt.Printf("------SHOULD NOT HAPPEN")
+		dblogger.Errorf("error in addTable:%s. Can't add a table for a node isn't known.", err)
 		return
 	}
 	tName := genTableName(n.nodeName, t, n.nodeDuration)
@@ -411,6 +435,9 @@ func (ntc *nestedTableCache) addNode(n *node) {
 	ntc.nodes[n.nodeIP] = n
 }
 
+// genTableName takes a name of a collector, a time and a duration, and
+// it creates a tablename for the relations that will hold the relevant captures.
+// it uses underscores as a field separator because they don't have any effect in SQL.
 func genTableName(colName string, date time.Time, ddm int) string {
 	dur := time.Duration(ddm) * time.Minute
 	trunctime := date.Truncate(dur).UTC()
