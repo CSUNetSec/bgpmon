4a4c396b7075eb03fc8f520ee5cf8bbfbb3424ba
Will Yingling <willyingling@gmail.com>
2019-05-07 10:17:17 -0600

    Added structs file

---
+++ db/dbstructs.go
@@ -0,0 +1,66 @@
+package db
+
+import (
+	"net"
+
+	"github.com/CSUNetSec/bgpmon/config"
+	"github.com/CSUNetSec/bgpmon/util"
+)
+
+// node is a representation of a machine that is used as a BGP vantage point. It can be either
+// a collector or a peer. If it is a collector, it is used to generate table names for captures
+// seen by that collector. It can also be geolocated. Nodes can be discovered from stored messages
+// or provided from a configuration file.
+type node struct {
+	name        string
+	ip          string
+	isCollector bool
+	duration    int
+	description string
+	coords      string
+	address     string
+}
+
+// newNode creates an empty node.
+func newNode() *node {
+	return &node{}
+}
+
+// nodeConfigFromNode creates a node configuration from a node.
+func (n *node) nodeConfigFromNode() config.NodeConfig {
+	return config.NodeConfig{
+		Name:                n.name,
+		IP:                  n.ip,
+		IsCollector:         n.isCollector,
+		DumpDurationMinutes: n.duration,
+		Description:         n.description,
+		Coords:              n.coords,
+		Location:            n.address,
+	}
+}
+
+// Capture represent a BGPCapture a row in the generated capture tables. It
+// describes a single BGP event.
+type Capture struct {
+	fromTable string // mostly debug
+	id        string // the capture_id that together with the table makes it unique
+	origin    int    // origin as
+	protoMsg  []byte // the protobuf blob
+}
+
+// CaptureTable represents a row in the main table. It describes
+// an existing table populated with BGPCaptures
+type CaptureTable struct {
+	name      string
+	collector string
+	span      util.Timespan
+}
+
+// Entity represents a row in the entities table. It describes a party interested
+// in particular BGP data, like the owner of a prefix.
+type Entity struct {
+	name          string
+	email         string
+	ownedOrigins  []int
+	ownedPrefixes []net.IPNet
+}
